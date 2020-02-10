621e3a7b9f917f7aed063311ceebc5a5807fd6ff
César González <cegonse@gmail.com>
2020-02-10 19:47:21 +0100

    Properly handle Unicode strings in test case and suite names (#18)

---
+++ spec/test_description.cpp
@@ -0,0 +1,12 @@
+#include <cest>
+
+
+describe("a Cest test suite", []() {
+    beforeEach([]() { });
+
+    afterEach([]() { });
+
+    it("can contain test cases", []() { });
+
+    it("can also have test cases with unicode symbols 😀", []() { });
+});
