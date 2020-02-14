fb2165a0dd0a735f7b847b7675b3676e3d3a1e85
Cesar Gonzalez <cegonse@gmail.com>
2020-02-14 13:43:59 +0100

    Execute single or group of tests if a pattern is passed to make

--- Makefile
+++ Makefile
@@ -3,6 +3,11 @@ all:
 	@rm -f test_summary.jsonl
 	@./framework/build.py
 
+%:
+	@rm -f log.xml
+	@rm -f test_summary.jsonl
+	@./framework/build.py $@
+
 clean:
 	@rm -rf build/*
 	@rm -f log.xml
--- framework/compilation/files.py
+++ framework/compilation/files.py
@@ -2,3 +2,11 @@ import os
 
 def all_files_in(directory):
     return os.listdir(directory)
+
+
+def read_file(file):
+    f = open(file, 'r')
+    data = f.read()
+    f.close()
+
+    return data
